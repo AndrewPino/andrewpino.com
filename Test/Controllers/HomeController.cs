1036468daa6918b675580ffc2c4b7bf1bc116f9e
Andrew Pino <andrew@localhost.localdomain>
2019-11-19 21:30:55 -0700

    first commit

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("Resume")]
+        public IActionResult Resume()
+        {
+            return View();
+        }
+        
+        [Route("Research")]
+        public IActionResult Research()
+        {
+            return View();
+        }
+        
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+        [Route("Projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Test/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('.form-wrapper button.submit').click(function(e){
+    // prevent default submit
+    e.preventDefault();
+});
